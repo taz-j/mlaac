5196d5b6d9a8dacac4b058fd1232d5a748c92d59
Tanya Gibbs <tanyagibbs@outlook.com.au>
2020-01-05 14:51:38 +1000

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  //firebase: {
+    //apiKey: '<your-key>',
+    //authDomain: '<your-project-authdomain>',
+    //databaseURL: '<your-database-URL>',
+    //projectId: 'mlaac-d2ec2',
+    //storageBucket: '<your-storage-bucket>',
+    //messagingSenderId: '<your-messaging-sender-id>'
+  //}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    //apiKey: '<your-key>',
+    //authDomain: '<your-project-authdomain>',
+    //databaseURL: '<your-database-URL>',
+    projectId: 'mlaac-d2ec2',
+    //storageBucket: '<your-storage-bucket>',
+    //messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
 
 /*
